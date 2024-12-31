cf76aa066cfed0b61b84f18f6191afe2beee129c
山﨑　博史 <>
2024-12-31 16:34:12 +0900

    js；コンストラクタでプロパティを持たせる（引数ありバージョン）

--- class.js
+++ class.js
@@ -1,16 +1,18 @@
 // クラスを定義する
 class Product {
     // コンストラクタを使ってインスタンス化するときに初期化する
-    constructor() {
+    constructor(name, price, category) {
         // インスタンス（オブジェクト）にプロパティを持たせる
-        this.name = 'シャンプー';
-        this.price = 500;
-        this.category = '生活雑貨';
+        this.name = name;
+        this.price = price;
+        this.category = category;
     }
 }
 
 // インスタンス化する
-const shampoo = new Product();
+const shampoo = new Product('シャンプー', '500', '生活雑貨');
+const coffee = new Product('コーヒー', '1500' , '飲料')
 
  // インスタンス（オブジェクト）の値を出力する
  console.log(shampoo);
+ console.log(coffee);
